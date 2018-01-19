18798e1652d9e870ac465bdb0d8709025390913f
Gabriel Ferreira <Gabrielferrer@outlook.com.br>
2018-01-19 17:07:00 -0200

    Alterações

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
+var socket = new WebSocket("ws://192.168.52.3:5001");
 var input = document.querySelector('input');
 var lista = document.querySelector('ul');
 var nome = prompt('Digite seu nome:')
-console.log(input);
 
 input.addEventListener('keydown', (e) => {
   if(e.key == "Enter") {
     let dados = {
+      name: 'mensagem',
       mensagem: input.value,
       user: nome || 'Sem nome'
     };
     console.log(dados)
     socket.send(JSON.stringify(dados));
   }
-})
-
-var socket = new WebSocket("ws://192.168.52.3:5001");
-console.log(socket);
+});
 
 socket.onopen = (e) => {
   console.log('Socket conectado com sucesso!');
 }
 
 socket.onmessage = (e) => {
+  console.log(e.data);
   var msg = document.createElement("li");
   var data = JSON.parse(e.data);
   msg.innerHTML = `<b>${data.user}:</b> ${data.mensagem}`;
--- server/index.js
+++ server/index.js
@@ -1,19 +1,13 @@
 var server = require('ws').Server;
 var s = new server({port: 5001});
 
+
 s.on('connection', (ws) => {
-
-  console.log('Conectado')
+  console.log('Conectado');
   ws.on('message', (message) => {
-    console.log("User: " + message);
-
-    s.clients.forEach(function e (client) {
-      client.send(message);
-    })
-
-    if(message == 'oi') {
-      ws.send('Oi, tudo bem?');
-    }
+      s.clients.forEach(function e (client) {
+        client.send(message);
+      })
   })
 
   console.log("One omore client connected");
